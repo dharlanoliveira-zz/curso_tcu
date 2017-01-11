5b11a0fbfb237b4f53bf00776670cc8244f4db9a
Dharlan Oliveira <dharlanoliveira@gmail.com>
2017-01-11 10:26:35 -0200

    * Evoluindo exemplos typescript

---
+++ 2-typescript/index.ts
@@ -0,0 +1,5 @@
+let somar = (x,y) => {
+  return x + y;
+}
+
+console.log(somar(8,22));
---
+++ 2-typescript/tipos.ts
@@ -0,0 +1,10 @@
+function tiposbasicos(){
+  let nome:string = "texto";
+  let checado:boolean = true;
+  let idade:number = 18;
+  let objeto:any = {
+    propriedade : 'valor'
+  }
+
+  console.log(nome);
+}
